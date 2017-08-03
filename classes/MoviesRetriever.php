8c8f02b114d6f1d2e3ac109802b119a02a6a2429
AntonZelenin <ntnzelenin@gmail.com>
2017-08-03 12:50:52 +0300

    more refactoring

--- classes/MoviesRetriever.php
+++ classes/MoviesRetriever.php
@@ -20,6 +20,37 @@ class MoviesRetriever
         return $this->movies;
     }
 
+    public function getMoviesByName(string $name) : array
+    {
+        $this->moviesByName($name);
+        $movies_actors = $this->getMoviesActors();
+
+        $this->distributeActors($movies_actors);
+
+        return $this->movies;
+    }
+
+    public function getMoviesByActor(string $actor) : array
+    {
+        $temp = explode(' ', trim($actor));
+
+        $first_name = $temp[0];
+        $last_name = $temp[1];
+
+        $movies_id = $this->moviesIdByActor($first_name, $last_name);
+
+        $this->getMoviesIn($movies_id);
+        $movies_actors = $this->getMoviesActors();
+
+        if (!isset($movies_actors)) {
+            return [];
+        }
+
+        $this->distributeActors($movies_actors);
+
+        return $this->movies;
+    }
+
     private function getMovies()
     {
         $query = $this->pdo->prepare('SELECT movies.id, movies.name, movies.year, formats.format
@@ -41,13 +72,16 @@ class MoviesRetriever
     {
         $movies_id = $this->getMoviesIdStr();
 
+        if (!isset($movies_actors)) {
+            return [];
+        }
+
         $query = $this->pdo->query("SELECT movies_actors.movie_id, actors.first_name, actors.last_name
             FROM webby_lab_task.movies_actors
             LEFT JOIN webby_lab_task.actors
             ON actors.id = actor_id
             WHERE movie_id IN ($movies_id)
             ");
-        $query->execute();
 
         return $query->fetchAll();
     }
@@ -59,14 +93,13 @@ class MoviesRetriever
         }
     }
 
-    public function getMoviesByName(string $name) : array
+    private function addActorToMovie(array $actor)
     {
-        $this->moviesByName($name);
-        $movies_actors = $this->getMoviesActors();
+        $movie_id = $actor['movie_id'];
+        $first_name = $actor['first_name'];
+        $last_name = $actor['last_name'];
 
-        $this->distributeActors($movies_actors);
-
-        return $this->movies;
+        $this->movies[$movie_id]->addActor(new Actor($first_name, $last_name));
     }
 
     private function moviesByName(string $name)
@@ -88,36 +121,6 @@ class MoviesRetriever
         }
     }
 
-    private function getMoviesIdStr()
-    {
-        $movies_id = '';
-        foreach ($this->movies as $key => $movie) {
-            $movies_id .= "$key,";
-        }
-
-        return substr($movies_id, 0, -1);
-    }
-
-    public function getMoviesByActor(string $actor) : array
-    {
-        $temp = explode(' ', trim($actor));
-        $first_name = $temp[0];
-        $last_name = $temp[1];
-
-        if (!isset($first_name) || !isset($last_name)) {
-            return [];
-        }
-
-        $movies_id = $this->moviesIdByActor($first_name, $last_name);
-
-        $this->getMoviesIn($movies_id);
-        $movies_actors = $this->getMoviesActors();
-
-        $this->distributeActors($movies_actors);
-
-        return $this->movies;
-    }
-
     private function moviesIdByActor(string $first_name, string $last_name) : string
     {
         $query = $this->pdo->prepare("SELECT movies_actors.movie_id
@@ -127,9 +130,29 @@ class MoviesRetriever
             WHERE first_name = :first_name
             AND last_name = :last_name
             ");
-        $query->execute(['first_name' => $first_name, 'last_name' => $last_name]);
+            $query->execute(['first_name' => $first_name, 'last_name' => $last_name]);
 
-        return $this->moviesIdToString($query->fetchAll());
+            return $this->moviesIdToString($query->fetchAll());
+    }
+
+    private function moviesIdToString(array $movies_id) : string
+    {
+        $movies_id_str = '';
+        foreach ($movies_id as $key => $value) {
+            $movies_id_str .= intval($value['movie_id']).',';
+        }
+
+        return substr($movies_id_str, 0, -1);
+    }
+
+    private function getMoviesIdStr()
+    {
+        $movies_id = '';
+        foreach ($this->movies as $key => $movie) {
+            $movies_id .= "$key,";
+        }
+
+        return substr($movies_id, 0, -1);
     }
 
     private function getMoviesIn($movies_id)
@@ -148,24 +171,5 @@ class MoviesRetriever
 
             $this->movies[$id] = $movie;
         }
-    }
-
-    private function moviesIdToString(array $movies_id) : string
-    {
-        $movies_id_str = '';
-        foreach ($movies_id as $key => $value) {
-            $movies_id_str .= intval($value['movie_id']).',';
-        }
-
-        return substr($movies_id_str, 0, -1);
-    }
-
-    private function addActorToMovie(array $actor)
-    {
-        $movie_id = $actor['movie_id'];
-        $first_name = $actor['first_name'];
-        $last_name = $actor['last_name'];
-
-        $this->movies[$movie_id]->addActor(new Actor($first_name, $last_name));
     }
 }
