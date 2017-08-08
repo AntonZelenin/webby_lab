b4f523356c73e5c1587ab2998a7f9acae50f7051
anton <anton@clientdc.gl>
2017-08-08 15:55:06 +0300

    UI

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
     <head>
         <title>Movies</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
         <link rel="stylesheet" type="text/css" href="/css/style.css" />
@@ -11,56 +12,53 @@
     </head>
 
     <body>
-        <div class="sidebar w3-sidebar">
-            <form action="/scripts/addMovie.php" method="post" >
-                <div class="add-movie">
-                    Movie Title:<br />
-                    <input type="text" class="textbox" name="name" placeholder="e.g. Matrix" required /><br />
-                    Release Year:<br />
-                    <input type="number" min='1900' class="textbox" max='2017' name="year" placeholder="e.g. 1999" required /><br />
-                    Format: <br />
-                    <input type="text" name="format" class="textbox" placeholder="e.g. DVD" required /><br />
-                    Stars:<br />
-                    <input type="text" name="actors" placeholder="e.g. Keanu Reeves, Laurence Fishburne" required /><br />
+        <div class="wrapper">
+            <div class="sidebar">
+                <form action="/scripts/addMovie.php" method="post" >
+                    <div class="form-wrapper">
+                        <input type="text" name="name" placeholder="Movie Title" required />
+
+                        <div class="year-format">
+                            <input type="number" class="year" min='1900' max='2017' name="year" placeholder="Release Year" required />
+                            <input type="text" name="format" placeholder="Format" required />
+                        </div>
+
+                        <input type="text" name="actors" placeholder="Stars (e.g. Keanu Reeves)" required />
+                        <input type="Submit" class="submit-button" value="Add Movie" />
+                    </div>
+
+                </form>
+
+                <div class="form-wrapper">
+                    <input type="text" name='name' placeholder="Search by name" required />
+                    <input type="button" class="submit-button" value="Search" onclick="submitForm(this.parentElement)" />
                 </div>
 
-                <input type="Submit" class="button" value="Add Movie" />
-            </form>
-
-
-            <div>
-                <div class="search">
-                    Search by movie name:<br />
-                    <input type="text" name='name' placeholder="e.g. Jaws" required />
+                <div class="form-wrapper">
+                    <input type="text" name='actor' placeholder="Search by actor" required />
+                    <input type="submit" value="Search" onclick="submitForm(this.parentElement)" />
                 </div>
-                <input type="button" class="button" value="Search" onclick="submitForm(this.parentElement)" />
-            </div>
 
-            <div>
-                <div class="search">
-                    Search by actor name:<br />
-                    <input type="text" name='actor' placeholder="e.g. Robert Shaw" required />
+                <div class="form-wrapper flex-row">
+                    <input type="button" value="Show all" onclick="reset()" />
+                    <input type="button" class="order" id="order" value="Order by name" onclick="order_list()">
                 </div>
-                <input type="submit" class="button" value="Search" onclick="submitForm(this.parentElement)" />
+
+                <form action="/scripts/upload.php" class="upload" method="post" enctype="multipart/form-data">
+                    Select file to upload:
+                    <input type="file" name="file" id="file" required />
+                    <input type="submit" value="Upload" name="submit" />
+                </form>
             </div>
 
-            <input type="button" class="button" value="Show all" onclick="reset()" />
-            <input type="button" id="order" value="Order by name" onclick="order_list()">
-
-            <form action="/scripts/upload.php" class="upload" method="post" enctype="multipart/form-data">
-                Select file to upload:
-                <input type="file" name="file" id="file" required />
-                <input type="submit" class="button" value="Upload" name="submit" />
-            </div>
+            <div class="main" id="main"></div>
         </div>
-
-        <div class="main" id="main"></div>
 
         <template id="movie-template">
             <div class="movie-wrapper">
                 <div class='movie'>
                     <span id='movie-name-year' onclick='showInfo(this.parentElement)'>$name ($year)</span>
-                    <div class='del' id='delete' onclick='del(this.parentElement)'>&times;</div>
+                    <div class='del' id='delete' onclick='del(this.parentElement)' title="Delete">&times;</div>
                 </div>
 
                 <div class='modal'>
